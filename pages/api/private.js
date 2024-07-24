4077ac5dcf6a3d1124836623822397742a226065
Yulyana Ramanenka <yul.ramanenka@gmail.com>
2024-07-24 21:46:41 +0200

    protect private api

--- pages/api/private.js
+++ pages/api/private.js
@@ -1,5 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import {
+  withApiAuthRequired,
+  getSession,
+  getAccessToken,
+} from "@auth0/nextjs-auth0";
 
-export default function handler(req, res) {
-  res.status(200).json({ message: 'Welcome to our private data' })
+export async function handler(req, res) {
+  try {
+    const session = await getSession(req, res);
+    const token = getAccessToken(req, res);
+    const user = session.user;
+
+    console.log("🚨[private.js:10]: session: ", session);
+    console.log("🚨[private.js:12]: token: ", token);
+    console.log("🚨[private.js:14]: user: ", user);
+
+    res.status(200).json({ message: "Welcome to our private data" });
+  } catch (err) {
+    res.status(401).json({ message: "Unauthorised" });
+  }
 }
+
+export default withApiAuthRequired(handler);
